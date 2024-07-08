4ae032e850b36e6a16494c082c149f4482fe6904
RodoFire <louisbriche@gmail.com>
2024-07-08 17:44:19 +0200

    block tester for features

--- src/main/java/net/rodofire/easierworldcreator/EasierworldcreatorDataGenerator.java
+++ src/main/java/net/rodofire/easierworldcreator/EasierworldcreatorDataGenerator.java
@@ -2,10 +2,18 @@ package net.rodofire.easierworldcreator;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
+import net.rodofire.easierworldcreator.devtest.ModConfiguredFeatures;
 
 public class EasierworldcreatorDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
+	}
+
+	@Override
+	public void buildRegistry(RegistryBuilder registryBuilder) {
+		registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
 	}
 }
