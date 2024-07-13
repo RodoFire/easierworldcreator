3fa33b2c81c0aa6625f13bf0fc240f838337f5c0
RodoFire <louisbriche@gmail.com>
2024-07-13 13:11:14 +0200

    circle gen finished

--- src/main/java/net/rodofire/easierworldcreator/worldgenutil/GenCircles.java
+++ src/main/java/net/rodofire/easierworldcreator/worldgenutil/GenCircles.java
@@ -1,31 +1,109 @@
 package net.rodofire.easierworldcreator.worldgenutil;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.StructureWorldAccess;
 
+import java.util.List;
+
 public class GenCircles {
     //Validate
-    public static void generateCircle(StructureWorldAccess world,  int largex, BlockState state, BlockPos pos) {
+    public static void generateCircle(StructureWorldAccess world, int radius, BlockState state, BlockPos pos) {
         int x = pos.getX();
         int y = pos.getY();
         int z = pos.getZ();
-        generateOval(world,  largex, largex, state, x, z, y, false, false);
-    }
-    public static void generateFullCircle(StructureWorldAccess world,  int largex, BlockState state, BlockPos pos) {
-        int x = pos.getX();
-        int y = pos.getY();
-        int z = pos.getZ();
-        generateOval(world,  largex, largex, state, x, z, y, false, true);
-    }
-    public static void generateFullCircle(StructureWorldAccess world,  int largex, BlockState state, BlockPos pos,boolean force) {
-        int x = pos.getX();
-        int y = pos.getY();
-        int z = pos.getZ();
-        generateOval(world,  largex, largex, state, x, z, y, force, true);
+        generateOval(world, radius, radius, x, z, y, false, false, null, List.of(state));
     }
 
-    public static void generateOval(StructureWorldAccess world, int radiusX, int radiusZ, BlockState blockState, int centerX, int centerZ, int y, boolean force, boolean full) {
+    public static void generateFullCircle(StructureWorldAccess world, int radius, BlockState state, BlockPos pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radius, radius, x, z, y, false, true, null, List.of(state));
+    }
+
+    public static void generateFullCircle(StructureWorldAccess world, int radius, BlockState state, BlockPos pos, boolean force) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radius, radius, x, z, y, force, true, null, List.of(state));
+    }
+
+    public static void generateFullCircle(StructureWorldAccess world, int radius, BlockPos pos, boolean force, List<Block> stateToForce, List<BlockState> stateToPlace) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radius, radius, x, z, y, force, true, stateToForce, stateToPlace);
+    }
+
+    public static void generateCircle(StructureWorldAccess world, int radius, List<BlockState> state, BlockPos pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radius, radius, x, z, y, false, false, null, state);
+    }
+
+    public static void generateFullCircle(StructureWorldAccess world, int radius, List<BlockState> state, BlockPos pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radius, radius, x, z, y, false, true, null, state);
+    }
+
+    public static void generateFullCircle(StructureWorldAccess world, int radius, List<BlockState> state, BlockPos pos, boolean force) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radius, radius, x, z, y, force, true, null, state);
+    }
+
+
+    public static void generateElipsoid(StructureWorldAccess world, int radiusx, int radiusz, BlockState state, BlockPos pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radiusx, radiusz, x, z, y, false, false, null, List.of(state));
+    }
+
+    public static void generateFullElipsoid(StructureWorldAccess world, int radiusx, int radiusz, BlockState state, BlockPos pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radiusx, radiusz, x, z, y, false, true, null, List.of(state));
+    }
+
+    public static void generateFullElipsoid(StructureWorldAccess world, int radiusx, int radiusz, BlockState state, BlockPos pos, boolean force) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radiusx, radiusz, x, z, y, force, true, null, List.of(state));
+    }
+
+    public static void generateElipsoid(StructureWorldAccess world, int radiusx, int radiusz, List<BlockState> state, BlockPos pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radiusx, radiusz, x, z, y, false, false, null, state);
+    }
+
+    public static void generateFullElipsoid(StructureWorldAccess world, int radiusx, int radiusz, List<BlockState> state, BlockPos pos) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radiusx, radiusz, x, z, y, false, true, null, state);
+    }
+
+    public static void generateFullElipsoid(StructureWorldAccess world, int radiusx, int radiusz, List<BlockState> state, BlockPos pos, boolean force) {
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+        generateOval(world, radiusx, radiusz, x, z, y, force, true, null, state);
+    }
+
+
+    /*---------- Algorithm based on Bressen Algorithms for circle ----------*/
+    public static void generateOval(StructureWorldAccess world, int radiusX, int radiusZ, int centerX, int centerZ, int y, boolean force, boolean full, List<Block> blockToForce, List<BlockState> blockToPlace) {
         int x = 0;
         int z = radiusZ;
         int twoASquare = 2 * radiusX * radiusX;
@@ -36,10 +114,10 @@ public class GenCircles {
 
         // Region 1
         while (dx < dz) {
-            if(!full) {
-                placeOvalBlocks(world, blockState, centerX, centerZ, x, y, z, force);
-            }else {
-                placeFullOval(world, blockState, centerX, centerZ, x, y, z, force);
+            if (!full) {
+                placeOvalBlocks(world, centerX, centerZ, x, y, z, force, blockToForce, blockToPlace);
+            } else {
+                placeFullOval(world, centerX, centerZ, x, y, z, force, blockToForce, blockToPlace);
             }
             if (decision1 < 0) {
                 x++;
@@ -57,10 +135,10 @@ public class GenCircles {
         // Region 2
         int decision2 = (int) (radiusZ * radiusZ * (x + 0.5) * (x + 0.5) + radiusX * radiusX * (z - 1) * (z - 1) - radiusX * radiusX * radiusZ * radiusZ);
         while (z >= 0) {
-            if(!full) {
-                placeOvalBlocks(world, blockState, centerX, centerZ, x, y, z, force);
-            }else {
-                placeFullOval(world, blockState, centerX, centerZ, x, y, z, force);
+            if (!full) {
+                placeOvalBlocks(world, centerX, centerZ, x, y, z, force, blockToForce, blockToPlace);
+            } else {
+                placeFullOval(world, centerX, centerZ, x, y, z, force, blockToForce, blockToPlace);
             }
             if (decision2 > 0) {
                 z--;
@@ -76,36 +154,36 @@ public class GenCircles {
         }
     }
 
-    public static void placeOvalBlocks(StructureWorldAccess world, BlockState blockstate, int centerX, int centerZ, int x, int y, int z, boolean force) {
+    public static void placeOvalBlocks(StructureWorldAccess world, int centerX, int centerZ, int x, int y, int z, boolean force, List<Block> blocksToForce, List<BlockState> blockToPlace) {
         BlockPos.Mutable pos = new BlockPos.Mutable();
-        if(!force){
-            if(world.getBlockState(pos.set(centerX + x, y, centerZ + z)).isAir()){
-                world.setBlockState(pos, blockstate, 2);
-            }
-            if(world.getBlockState(pos.set(centerX + x, y, centerZ - z)).isAir()){
-                world.setBlockState(pos, blockstate, 2);
-            }
-            if (world.getBlockState(pos.set(centerX - x, y, centerZ + z)).isAir()){
-                world.setBlockState(pos, blockstate, 2);
-            }
-            if (world.getBlockState(pos.set(centerX - x, y, centerZ - z)).isAir()){
-                world.setBlockState(pos, blockstate, 2);
-            }
-        }else {
-            world.setBlockState(pos.set(centerX + x, y, centerZ + z), blockstate, 2);
-            world.setBlockState(pos.set(centerX + x, y, centerZ - z), blockstate, 2);
-            world.setBlockState(pos.set(centerX - x, y, centerZ + z), blockstate, 2);
-            world.setBlockState(pos.set(centerX - x, y, centerZ - z), blockstate, 2);
-        }
+        int length = blockToPlace.size() - 1;
+        WorldGenUtil.verifyBlock(world, force, blocksToForce, blockToPlace, pos.set(centerX + x, 0, centerZ + z), length);
+        WorldGenUtil.verifyBlock(world, force, blocksToForce, blockToPlace, pos.set(centerX + x, 0, centerZ - z), length);
+        WorldGenUtil.verifyBlock(world, force, blocksToForce, blockToPlace, pos.set(centerX - x, 0, centerZ + z), length);
+        WorldGenUtil.verifyBlock(world, force, blocksToForce, blockToPlace, pos.set(centerX - x, 0, centerZ - z), length);
 
     }
-    public static void placeFullOval(StructureWorldAccess world, BlockState blockstate, int centerX, int centerZ, int x, int y, int z, boolean force){
+
+    //place lines between the blocks
+    public static void placeFullOval(StructureWorldAccess world, int centerX, int centerZ, int x, int y, int z, boolean force, List<Block> blocksToForce, List<BlockState> blocksToPlace) {
         BlockPos start1 = new BlockPos(centerX + x, y, centerZ + z);
         BlockPos start2 = new BlockPos(centerX - x, y, centerZ + z);
 
         BlockPos end1 = new BlockPos(centerX + x, y, centerZ - z);
         BlockPos end2 = new BlockPos(centerX - x, y, centerZ - z);
-        GenLines.drawLine(world,start1,end1,blockstate,force);
-        GenLines.drawLine(world,start2,end2,blockstate,force);
+
+        int length = blocksToPlace.size() - 1;
+
+        BlockPos.Mutable mutable = new BlockPos.Mutable();
+        System.out.println(end2 + "  " + end1);
+        for (int i = 0; i <= 2 * z; i++) {
+            mutable.set(start1, 0, 0, -i);
+            WorldGenUtil.verifyBlock(world, force, blocksToForce, blocksToPlace, mutable, length);
+            mutable.set(start2, 0, 0, -i);
+            WorldGenUtil.verifyBlock(world, force, blocksToForce, blocksToPlace, mutable, length);
+
+        }
+        //GenLines.drawLine(world, start1, end1, blockstate.get(Random.create().nextBetween(0,length)), force);
+        //GenLines.drawLine(world, start2, end2, blockstate.get(Random.create().nextBetween(0,length)), force);
     }
 }
