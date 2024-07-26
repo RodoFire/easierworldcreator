cde3b66ab636f2b15d00ac0b1a6ea50c78c76963
RodoFire <louisbriche@gmail.com>
2024-07-26 11:57:02 +0200

    new methods and method changes

--- src/main/java/net/rodofire/easierworldcreator/worldgenutil/WorldGenUtil.java
+++ src/main/java/net/rodofire/easierworldcreator/worldgenutil/WorldGenUtil.java
@@ -44,7 +44,7 @@ public class WorldGenUtil {
         return (Random.create().nextBetween(0, 1) == 0) ? 1 : -1;
     }
 
-    public static boolean getRandomBoolean(float chance){
+    public static boolean getRandomBoolean(float chance) {
         return Random.create().nextFloat() < chance;
     }
 
@@ -66,23 +66,45 @@ public class WorldGenUtil {
     public static int getSecondHeight(float chance, int maxheight) {
         return getSecondHeight(chance, 0, maxheight);
     }
+
     public static int getSecondHeight(float chance, int minheight, int maxheight) {
-        if(Random.create().nextFloat() < chance) {
+        if (Random.create().nextFloat() < chance) {
             return Random.create().nextBetween(minheight, maxheight);
         }
         return 0;
     }
 
-    //method to verify that the block is not an unbreakable block or not and to verify if the block can be put or not
-    public static void verifyBlock(StructureWorldAccess world, boolean force, List<Block> blocksToForce, List<BlockState> blocksToPlace, BlockPos pos) {
+    //method to verify that the block is not an unbreakable block or not and to verify if the block can be put or not. If the block was placed, it returns true
+    public static boolean verifyBlock(StructureWorldAccess world, boolean force, List<Block> blocksToForce, List<BlockState> blocksToPlace, BlockPos pos) {
         BlockState state2 = world.getBlockState(pos);
-        int length = blocksToPlace.size()-1;
-        if (state2.getHardness(world, pos) < 0) return ;
+        int length = blocksToPlace.size() - 1;
+        if (state2.getHardness(world, pos) < 0) return false;
         if (!force) {
-            if(blocksToForce == null) blocksToForce = List.of(Blocks.BEDROCK);
-            if (!state2.isAir() && blocksToForce.stream().noneMatch(state2.getBlock()::equals)) return ;
+            if (blocksToForce == null) blocksToForce = List.of(Blocks.BEDROCK);
+            if (!state2.isAir() && blocksToForce.stream().noneMatch(state2.getBlock()::equals)) return false;
         }
         world.setBlockState(pos, blocksToPlace.get(Random.create().nextBetween(0, length)), 2);
+        return true;
+    }
+
+    public static boolean isBlockInBlockStateList(Block block, List<BlockState> state) {
+        // Parcourir la liste des BlockState pour vérifier si le bloc correspond
+        for (BlockState blockState : state) {
+            if (blockState.getBlock().equals(block)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static List<Block> addBlockStateListtoBlockList(List<Block> block, List<BlockState> state) {
+        List<Block> secondlist = new ArrayList<>();
+        if (block == null) block = new ArrayList<>();
+        for (BlockState blockState : state) {
+            secondlist.add(blockState.getBlock());
+        }
+        block.addAll(secondlist);
+        return block;
     }
 
 
