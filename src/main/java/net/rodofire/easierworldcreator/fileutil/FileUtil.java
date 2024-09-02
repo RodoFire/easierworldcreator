808910d1556f7750e51d0387ad19cd3c6cc2c475
RodoFire <louisbriche@gmail.com>
2024-09-02 13:34:56 +0200

    remove directory method addition

--- src/main/java/net/rodofire/easierworldcreator/fileutil/FileUtil.java
+++ src/main/java/net/rodofire/easierworldcreator/fileutil/FileUtil.java
@@ -1,11 +1,15 @@
 package net.rodofire.easierworldcreator.fileutil;
 
+import net.minecraft.util.WorldSavePath;
+import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.chunk.Chunk;
 import net.rodofire.easierworldcreator.Easierworldcreator;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Objects;
 
 public class FileUtil {
     public static void renameFile(Path oldPath, Path newPath) {
@@ -25,6 +29,27 @@ public class FileUtil {
             }
         } else {
             Easierworldcreator.LOGGER.warn("Renaming file failed, file doesn't exist: " + oldPath.toString());
+        }
+    }
+
+    public static void removeFile(Path path) {
+        File file = new File(path.toString());
+        if (file.exists()) {
+            boolean deleted = file.delete();
+        } else {
+        }
+    }
+
+    public static Path getGeneratedChunkDirectory(Chunk chunk, StructureWorldAccess world) {
+        Path generatedPath = Objects.requireNonNull(world.getServer()).getSavePath(WorldSavePath.GENERATED).normalize();
+        String chunkDirPrefix = "chunk_" + chunk.getPos().x + "_" + chunk.getPos().z;  // Prefix to match chunk directories
+        return generatedPath.resolve(Easierworldcreator.MOD_ID).resolve("structures").resolve(chunkDirPrefix);
+    }
+    public static void removeGeneratedChunkDirectory(Chunk chunk, StructureWorldAccess world) {
+        Path directoryPath = getGeneratedChunkDirectory(chunk, world);
+        File file = new File(directoryPath.toString());
+        if(file.exists() && file.isDirectory()) {
+            boolean deleted = file.delete();
         }
     }
 }
