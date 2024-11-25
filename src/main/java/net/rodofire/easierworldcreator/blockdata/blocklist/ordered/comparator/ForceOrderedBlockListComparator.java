9cd971b2d5cdd39aea5b7d061f004315ce91e8ac
RodoFire <louisbriche@gmail.com>
2024-11-25 19:06:08 +0100

    overrided some DefaultOrderedBlockList methods

--- src/main/java/net/rodofire/easierworldcreator/blockdata/blocklist/ordered/comparator/ForceOrderedBlockListComparator.java
+++ src/main/java/net/rodofire/easierworldcreator/blockdata/blocklist/ordered/comparator/ForceOrderedBlockListComparator.java
@@ -16,6 +16,21 @@ import java.util.Set;
 /**
  * Class to manage an ordered list with force parameters.
  * This class uses a BiMap that connects each BlockState to a pair of Boolean and Set.
+ * Force parameters works like that:
+ * <pre>
+ *     {@code
+ * worldBlock : block at the blockPos
+ * placeBlock : block to place at the blockPos
+ * if force :
+ *      place placeBlock
+ * else if worldBlock == air :
+ *      place placeBlock
+ * else if worldBlock in forceBlock :
+ *      place placeBlock
+ * else :
+ *      dont place
+ *}
+ * </pre>
  */
 @SuppressWarnings("unused")
 public class ForceOrderedBlockListComparator extends DefaultOrderedBlockListComparator {
@@ -193,29 +208,104 @@ public class ForceOrderedBlockListComparator extends DefaultOrderedBlockListComp
         this.forceBlocks.get(getStateIndex(state)).setLeft(force);
     }
 
+    /**
+     * Method to place the block related to the index.
+     * The method also performs verification to know if the block can be placed.
+     *
+     * @param world the world the block will be placed
+     * @param index the index of the block
+     * @return true if the block was placed, false if not
+     */
     @Override
-    public boolean placeFirst(StructureWorldAccess world) {
-        short index = this.posMap.get(this.getFirstBlockPos());
+    public boolean placeWithVerification(StructureWorldAccess world, int index) {
+        Pair<BlockPos, BlockState> data = this.getPosPair(index);
+        BlockPos pos = data.getLeft();
+        BlockState state = data.getRight();
+        short ind = this.statesMap.inverse().get(state);
+        return BlockPlaceUtil.placeVerifiedBlock(world, this.forceBlocks.get(ind).getLeft(), this.forceBlocks.get(ind).getRight(), pos, state);
+    }
+
+    /**
+     * Method to place the block with the deletion of the BlockPos
+     * The method also performs verification to know if the block can be placed.
+     *
+     * @param world the world the block will be placed
+     * @param index the index of the block
+     * @return true if the block was placed, false if not
+     */
+    @Override
+    public boolean placeWithVerificationDeletion(StructureWorldAccess world, int index) {
+        Pair<BlockPos, BlockState> data = this.removeBlockPosPair(index);
+        BlockPos pos = data.getLeft();
+        BlockState state = data.getRight();
+        short ind = this.statesMap.inverse().get(state);
+        return BlockPlaceUtil.placeVerifiedBlock(world, this.forceBlocks.get(ind).getLeft(), this.forceBlocks.get(ind).getRight(), pos, state);
+    }
+
+    /**
+     * Method to place the last Block.
+     *
+     * @param world the world the last block will be placed
+     *              The method also performs verification to know if the block can be placed.
+     * @return true if the block was placed, false if not
+     */
+    @Override
+    public boolean placeLastWithVerification(StructureWorldAccess world) {
+        Pair<BlockPos, BlockState> data = this.getLastPosPair();
+        BlockPos pos = data.getLeft();
+        BlockState state = data.getRight();
+        short ind = this.statesMap.inverse().get(state);
+        return BlockPlaceUtil.placeVerifiedBlock(world, this.forceBlocks.get(ind).getLeft(), this.forceBlocks.get(ind).getRight(), pos, state);
+    }
+
+    /**
+     * Method to place the last Block of the comparator and removing it then.
+     * The method also performs verification to know if the block can be placed.
+     * Consider using this method because it gives you better performance.
+     *
+     * @param world the world the last block will be placed
+     * @return true if the block was placed, false if not
+     */
+    @Override
+    public boolean placeLastWithVerificationDeletion(StructureWorldAccess world) {
+        Pair<BlockPos, BlockState> data = this.removeLastBlockPosPair();
+        BlockPos pos = data.getLeft();
+        BlockState state = data.getRight();
+        short ind = this.statesMap.inverse().get(state);
+        return BlockPlaceUtil.placeVerifiedBlock(world, this.forceBlocks.get(ind).getLeft(), this.forceBlocks.get(ind).getRight(), pos, state);
+    }
+
+    /**
+     * Method to place the first Block.
+     * <p>The method also performs verification to know if the block can be placed.
+     *
+     * @param world the world where the block will be placed
+     * @return true if the block was placed, false if not.
+     */
+    @Override
+    public boolean placeFirstWithVerification(StructureWorldAccess world) {
         Pair<BlockPos, BlockState> data = this.getFirstPosPair();
         BlockPos pos = data.getLeft();
         BlockState state = data.getRight();
-        return BlockPlaceUtil.placeVerifiedBlock(world, this.forceBlocks.get(index).getLeft(), this.forceBlocks.get(index).getRight(), pos, state);
+        short ind = this.statesMap.inverse().get(state);
+        return BlockPlaceUtil.placeVerifiedBlock(world, this.forceBlocks.get(ind).getLeft(), this.forceBlocks.get(ind).getRight(), pos, state);
     }
 
+    /**
+     * <p>Method to place the first Block and deleting it.
+     * <p>The method also performs verification to know if the block can be placed.
+     * <p>You shouldn't use this method in normal case since that the method is pretty costly O(n).
+     * <p>Use instead {@code placeLastWithDeletion()} that is faster O(1).
+     *
+     * @param world the world where the block will be placed
+     * @return true if the block was placed, false if not.
+     */
     @Override
-    public boolean placeFirstWithDeletion(StructureWorldAccess world) {
-        short index = this.posMap.get(this.getFirstBlockPos());
+    public boolean placeFirstWithVerificationDeletion(StructureWorldAccess world) {
         Pair<BlockPos, BlockState> data = this.removeFirstBlockPos();
         BlockPos pos = data.getLeft();
         BlockState state = data.getRight();
-        return BlockPlaceUtil.placeVerifiedBlock(world, this.forceBlocks.get(index).getLeft(), this.forceBlocks.get(index).getRight(), pos, state);
-    }
-
-    @Override
-    public boolean place(StructureWorldAccess world, int index) {
-        BlockPos pos = this.getBlockPos(index);
-        short sh = this.posMap.get(pos);
-        BlockState data = this.getBlockState(sh);
-        return BlockPlaceUtil.placeVerifiedBlock(world, this.forceBlocks.get(sh).getLeft(), this.forceBlocks.get(sh).getRight(), pos, data);
+        short ind = this.statesMap.inverse().get(state);
+        return BlockPlaceUtil.placeVerifiedBlock(world, this.forceBlocks.get(ind).getLeft(), this.forceBlocks.get(ind).getRight(), pos, state);
     }
 }
