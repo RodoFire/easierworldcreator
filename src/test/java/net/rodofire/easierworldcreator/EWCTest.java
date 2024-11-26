86a257040cedf02bdf535e8b29c59c88701f90cd
RodoFire <louisbriche@gmail.com>
2024-11-26 13:29:21 +0100

    created custom run configuration for tests

--- src/test/java/net/rodofire/easierworldcreator/EWCTest.java
+++ src/test/java/net/rodofire/easierworldcreator/EWCTest.java
@@ -5,9 +5,12 @@ import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
+import net.rodofire.easierworldcreator.blockdata.sorter.BlockSortedTest;
 import net.rodofire.easierworldcreator.devtest.FeaturesRelated;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public class EWCTest implements ModInitializer {
     public static final String MOD_ID = "ewc-test";
@@ -15,12 +18,17 @@ public class EWCTest implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        BlockSortedTest.testSorting();
         FeaturesRelated.ModItems.registerModItems();
         FeaturesRelated.ModBlocks.registerModBlocks();
         FeaturesRelated.ModFeatures.addFeatures();
         BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.PLAINS), GenerationStep.Feature.TOP_LAYER_MODIFICATION, FeaturesRelated.ModPLacedFeatures.FEATURE_TESTER);
 
         EasierWorldCreator.init();
+        if (Objects.equals(System.getenv("enableTests"), "true")) {
+            LOGGER.info("starting tests");
+            Tests.registerTests();
+        }
         LOGGER.info("starting ewc test");
     }
 }
