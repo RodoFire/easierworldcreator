72667d1b14d7a12366ade29ee91e498c2aedf654
RodoFire <louisbriche@gmail.com>
2025-01-03 11:17:52 +0100

    1.21.4 update

--- src/main/java/net/rodofire/easierworldcreator/config/client/ModMenuConfigScreen.java
+++ src/main/java/net/rodofire/easierworldcreator/config/client/ModMenuConfigScreen.java
@@ -3,6 +3,8 @@ package net.rodofire.easierworldcreator.config.client;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.rodofire.easierworldcreator.EasierWorldCreator;
+import net.rodofire.easierworldcreator.config.ewc.EwcConfig;
+import net.rodofire.easierworldcreator.config.ewc.EwcConfigScreen;
 
 import java.util.Map;
 
@@ -10,7 +12,7 @@ public class ModMenuConfigScreen implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> ConfigScreen.getScreen(parent, EasierWorldCreator.MOD_ID);
+        return parent -> new EwcConfigScreen(parent, EwcConfig.MOD_CONFIG, EasierWorldCreator.MOD_ID);
     }
 
     @Override
