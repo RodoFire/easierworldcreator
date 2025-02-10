bff3db7d7afc0ec6c95b9c4888158a6c3e4741c8
RodoFire <louisbriche@gmail.com>
2025-02-10 22:44:09 +0100

    modified some method parameters

--- src/main/java/net/rodofire/easierworldcreator/shape/block/layer/Layer.java
+++ src/main/java/net/rodofire/easierworldcreator/shape/block/layer/Layer.java
@@ -1,10 +1,10 @@
 package net.rodofire.easierworldcreator.shape.block.layer;
 
 import it.unimi.dsi.fastutil.longs.AbstractLongCollection;
-import it.unimi.dsi.fastutil.longs.AbstractLongList;
 import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.StructureWorldAccess;
 import net.rodofire.easierworldcreator.blockdata.blocklist.BlockListManager;
 import net.rodofire.easierworldcreator.blockdata.blocklist.DividedBlockListManager;
 
@@ -14,32 +14,39 @@ import java.util.Map;
 public interface Layer {
     BlockListManager get(Map<ChunkPos, LongOpenHashSet> posMap);
 
-    void place(Map<ChunkPos, LongOpenHashSet> posMap);
+    void place(StructureWorldAccess world, Map<ChunkPos, LongOpenHashSet> posMap);
 
-    BlockListManager getVerified(Map<ChunkPos, LongOpenHashSet> posMap);
+    BlockListManager getVerified(StructureWorldAccess world, Map<ChunkPos, LongOpenHashSet> posMap);
 
     DividedBlockListManager getDivided(Map<ChunkPos, LongOpenHashSet> posMap);
 
-    DividedBlockListManager getVerifiedDivided(Map<ChunkPos, LongOpenHashSet> posMap);
+    DividedBlockListManager getVerifiedDivided(StructureWorldAccess world, Map<ChunkPos, LongOpenHashSet> posMap);
 
     <T extends Collection<BlockPos>> BlockListManager get(T posList);
 
-    <T extends Collection<BlockPos>> void place(T posList);
+    <T extends Collection<BlockPos>> void place(StructureWorldAccess world, T posList);
 
-    <T extends Collection<BlockPos>> BlockListManager getVerified(T posList);
+    <T extends Collection<BlockPos>> BlockListManager getVerified(StructureWorldAccess world, T posList);
 
     <T extends Collection<BlockPos>> DividedBlockListManager getDivided(T posList);
 
-    <T extends Collection<BlockPos>> DividedBlockListManager getVerifiedDivided(T posList);
+    <T extends Collection<BlockPos>> DividedBlockListManager getVerifiedDivided(StructureWorldAccess world, T posList);
 
     <U extends AbstractLongCollection> BlockListManager get(U posList);
 
-    <U extends AbstractLongCollection> void place(U posList);
+    <U extends AbstractLongCollection> void place(StructureWorldAccess world, U posList);
 
-    <U extends AbstractLongCollection> BlockListManager getVerified(U posList);
+    <U extends AbstractLongCollection> BlockListManager getVerified(StructureWorldAccess world, U posList);
 
     <U extends AbstractLongCollection> DividedBlockListManager getDivided(U posList);
 
-    <U extends AbstractLongCollection> DividedBlockListManager getVerifiedDivided(U posList);
+    <U extends AbstractLongCollection> DividedBlockListManager getVerifiedDivided(StructureWorldAccess world, U posList);
 
+    long getCenterPos();
+
+    void setCenterPos(long centerPos);
+
+    long getDirectionVector();
+
+    void setDirectionVector(long directionVector);
 }
