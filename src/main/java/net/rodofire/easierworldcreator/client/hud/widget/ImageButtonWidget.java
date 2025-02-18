12542c6ccc9837c1c9ce7cc732f082ec85623fe9
RodoFire <louisbriche@gmail.com>
2025-02-18 19:56:18 +0100

    various changes

--- src/main/java/net/rodofire/easierworldcreator/client/hud/widget/ImageButtonWidget.java
+++ src/main/java/net/rodofire/easierworldcreator/client/hud/widget/ImageButtonWidget.java
@@ -1,10 +1,8 @@
 package net.rodofire.easierworldcreator.client.hud.widget;
 
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
