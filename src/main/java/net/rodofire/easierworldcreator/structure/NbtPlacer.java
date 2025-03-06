71f431a968cab4b7a28de273d310e3749284a4b6
RodoFire <louisbriche@gmail.com>
2025-03-06 13:18:27 +0100

    method removal

--- src/main/java/net/rodofire/easierworldcreator/structure/NbtPlacer.java
+++ src/main/java/net/rodofire/easierworldcreator/structure/NbtPlacer.java
@@ -1,16 +1,12 @@
 package net.rodofire.easierworldcreator.structure;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.entity.StructureBlockBlockEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.structure.StructurePlacementData;
 import net.minecraft.structure.StructureTemplate;
 import net.minecraft.structure.StructureTemplateManager;
 import net.minecraft.structure.processor.BlockRotStructureProcessor;
-import net.minecraft.util.BlockMirror;
-import net.minecraft.util.BlockRotation;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.InvalidIdentifierException;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.StructureWorldAccess;
@@ -19,7 +15,6 @@ import net.rodofire.easierworldcreator.blockdata.blocklist.BlockListManager;
 import net.rodofire.easierworldcreator.shape.block.placer.animator.StructurePlaceAnimator;
 
 import java.util.Optional;
-import java.util.Set;
 
 /**
  * Class to place nbt structures in the world. To use a NbtPlacer, you can use this code:
@@ -96,11 +91,15 @@ public class NbtPlacer {
      * @param ignoreEntities ignore entities of the structure
      */
     public void place(float integrity, BlockPos pos, BlockPos offset, BlockMirror mirror, BlockRotation rotation, boolean ignoreEntities) {
-        this.place(integrity, pos, offset, mirror, rotation, ignoreEntities, true, null, null);
+        Pair<StructureTemplate, StructurePlacementData> data = processCommon(integrity, mirror, rotation, ignoreEntities);
+
+        if (data != null) {
+            data.getLeft().place(world, pos, offset, data.getRight(), world.getRandom(), 3);
+        }
     }
 
     /**
-     * this method allows you to place a structure in the world during world gen or not
+     * method to get a {@link BlockListManager} based on a structure
      *
      * @param integrity      the integrity of the structure must be between 0f and 1f
      * @param pos            the pos of the structure
@@ -108,52 +107,52 @@ public class NbtPlacer {
      * @param mirror         the block mirror if wanted
      * @param rotation       the structure rotation if wanted
      * @param ignoreEntities ignore entities of the structure
-     * @param force          determines if each block has to replace the already existing block if it exists
-     * @param blockToForce   set of block that the structure can still force in the case force = false
-     * @param blockToSkip    the list of blocks to skip during the pos
+     * @return the Structure converted into {@link BlockListManager}
      */
-    public void place(float integrity, BlockPos pos, BlockPos offset, BlockMirror mirror, BlockRotation rotation, boolean ignoreEntities, boolean force, Set<Block> blockToForce, Set<Block> blockToSkip) {
+    public BlockListManager get(float integrity, BlockPos pos, BlockPos offset, BlockMirror mirror, BlockRotation rotation, boolean ignoreEntities) {
+        BlockListManager manager = new BlockListManager();
+        Pair<StructureTemplate, StructurePlacementData> data = processCommon(integrity, mirror, rotation, ignoreEntities);
+
+        if (data != null) {
+            StructureUtil.convertNbtToManager(data.getLeft(), manager, data.getRight(), world, offset);
+        }
+        return manager;
+    }
+
+
+
+    private Pair<StructureTemplate, StructurePlacementData> processCommon(float integrity, BlockMirror mirror, BlockRotation rotation, boolean ignoreEntities) {
         MinecraftServer server = world.getServer();
         if (server == null) {
             Ewc.LOGGER.error("cannot get structure template, MinecraftServer is null. Structure template: {}", templateName);
-            return;
+            return null;
         }
         StructureTemplateManager structureTemplateManager = server.getStructureTemplateManager();
-        if (structureTemplateManager == null) return;
+        if (structureTemplateManager == null) return null;
 
         Optional<StructureTemplate> optional;
         try {
             optional = structureTemplateManager.getTemplate(templateName);
         } catch (InvalidIdentifierException var6) {
-            return;
+            Ewc.LOGGER.error("cannot get Nbt from Identifier");
+            var6.fillInStackTrace();
+            return null;
         }
 
-        //if the structure exists, we initialize the structure placement
-        if (optional.isPresent()) {
-            StructurePlacementData structurePlacementData = new StructurePlacementData()
-                    .setMirror(mirror)
-                    .setRotation(rotation)
-                    .setIgnoreEntities(ignoreEntities);
-            if (integrity < 1.0F) {
-                structurePlacementData.clearProcessors()
-                        .addProcessor(new BlockRotStructureProcessor(MathHelper.clamp(integrity, 0.0F, 1.0F)))
-                        .setRandom(StructureBlockBlockEntity.createRandom(world.getSeed()));
-            }
-            StructureTemplate structureTemplate = optional.get();
+        if (optional.isEmpty()) return null;
 
-            boolean bl2 = blockToForce == null || blockToForce.isEmpty();
-            boolean bl3 = blockToSkip == null || blockToSkip.isEmpty();
+        StructurePlacementData structurePlacementData = new StructurePlacementData()
+                .setMirror(mirror)
+                .setRotation(rotation)
+                .setIgnoreEntities(ignoreEntities);
 
-
-            if (animator != null || !bl2 || !bl3) {
-                    BlockListManager comparator = new BlockListManager();
-                    StructureUtil.convertNbtToManager(structureTemplate, comparator, structurePlacementData, world, offset);
-                    StructureUtil.place(world, animator, comparator, pos, force, blockToForce, blockToSkip, 1.0f);
-
-            } else {
-                structureTemplate.place(world, pos, offset, structurePlacementData, world.getRandom(), 3);
-            }
+        if (integrity < 1.0F) {
+            structurePlacementData.clearProcessors()
+                    .addProcessor(new BlockRotStructureProcessor(MathHelper.clamp(integrity, 0.0F, 1.0F)))
+                    .setRandom(StructureBlockBlockEntity.createRandom(world.getSeed()));
         }
 
+        return new Pair<>(optional.get(), structurePlacementData);
     }
+
 }
