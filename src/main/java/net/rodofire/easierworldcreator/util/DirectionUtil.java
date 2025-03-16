a5ff30d478fded4de736a7ef4ceac02f5612b17c
RodoFire <louisbriche@gmail.com>
2025-03-16 15:56:27 +0100

    util class

---
+++ src/main/java/net/rodofire/easierworldcreator/util/DirectionUtil.java
@@ -0,0 +1,57 @@
+package net.rodofire.easierworldcreator.util;
+
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.random.Random;
+
+public class DirectionUtil {
+    /**
+     * method to know
+     * @param dir
+     * @return
+     */
+    public static boolean isHorizontal(Direction dir) {
+        return switch (dir) {
+            case NORTH, SOUTH: yield false;
+            default: yield true;
+        };
+    }
+
+    /**
+     * method to get a random direction no matter the plane
+     *
+     * @return a random direction
+     */
+    public static Direction getRandomDirection() {
+        return switch (Random.create().nextBetween(0, 5)) {
+            case 0 -> Direction.WEST;
+            case 1 -> Direction.EAST;
+            case 2 -> Direction.NORTH;
+            case 3 -> Direction.SOUTH;
+            case 4 -> Direction.UP;
+            default -> Direction.DOWN;
+        };
+    }
+
+    /**
+     * method to get a random direction on the vertical axis
+     *
+     * @return a random direction on the vertical axis
+     */
+    public static Direction getRandomVerticalDirection() {
+        return Random.create().nextBetween(0, 1) == 1 ? Direction.UP : Direction.DOWN;
+    }
+
+    /**
+     * method to get a random direction on the horizontal axis
+     *
+     * @return a random direction on the horizontal axis
+     */
+    public static Direction getRandomHorizontalDirection() {
+        return switch (Random.create().nextBetween(0, 3)) {
+            case 0 -> Direction.WEST;
+            case 1 -> Direction.EAST;
+            case 2 -> Direction.NORTH;
+            default -> Direction.SOUTH;
+        };
+    }
+}
